763e8ddfacc5d6572694c8365148804c337fbf6b
Amol <amol.mane91@gmail.com>
2026-03-15 01:06:45 +0530

    NumberPattern JavaScript

--- NumberPattern.java
+++ NumberPattern.java
@@ -1,19 +1,14 @@
-package v;
+let num = 1
 
-public class NumberPattern {
-    public static void main(String[] args) {
-        int num = 1;  
-        
-        
-        for (int i = 1; i <= 5; i++) {
-            
-             
-            for (int j = 1; j <= i; j++) {
-                System.out.print(num + " ");  
-                num++;  
-            }
-            
-            System.out.println();  
-        }
-    }
+for(let i =1; i<=5 ; i++)
+{
+let row = "";
+ 
+for(let j = 0; j <= i; j++)
+{
+row += num + " ";
+num++;
+}
+
+console.log(row.trim());
 }
